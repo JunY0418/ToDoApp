bf0b0a9ab6e67c56b5527e8d8b4e3c1e38d513b1
JunY <make.the.best.of.destiny@gmail.com>
2023-05-03 12:06:21 +0900

    boardモデルの作成と実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  has_many :boards, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  def has_written?(board)
+    boards.exists?(id: board.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get '/users/sign_out' => 'devise/sessions#destroy'
+  # get '/users/sign_out' => 'devise/sessions#destroy'
+  root to: 'boards#index'
+  # get '/new', to: 'boards#new'
+  resources :boards
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
