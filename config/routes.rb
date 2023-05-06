c75ba3f701decfd6f4b9f5c41777ec9f09c8c0a8
JunY <make.the.best.of.destiny@gmail.com>
2023-05-07 01:05:08 +0900

    after_sign_up_path_for

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+    }
+  get '/users/:id', to: 'users#show', as: 'user'
   # get '/users/sign_out' => 'devise/sessions#destroy'
   root to: 'boards#index'
   # get '/new', to: 'boards#new'
   resources :boards
   resource :profile, only: [:show, :edit, :update]
+  post '/profile/edit', to: 'profiles#update'
+  # get '/profile/edit' => 'profiles#edit'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
